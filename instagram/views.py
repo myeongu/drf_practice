0c87cd68a19d01ebc8842f531fa182cdbf6c9297
myeongu <ausrndi@gmail.com>
2022-07-19 19:08:36 +0900

    ViewSet과 Router

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -33,11 +33,25 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()  # data 범위 지정(all, filter, ...)
     serializer_class = PostSerializer
 
+    @action(detail=False, methods=["GET"])
+    def public(self, request):
+        qs = self.get_queryset().filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["PATCH"])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        instance.is_public = True
+        instance.save(update_fields=["is_public"])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
     # Class 기반 view에서 요청이 될 때마다 호출되는 함수
-    def dispatch(self, request, *args, **kwargs):
-        print("request body :", request.body)  # 실제 서비스라면 logger(장고 기본 지원)을 사용 추천
-        print("request POST :", request.POST)
-        return super().dispatch(request, *args, **kwargs)
+    # def dispatch(self, request, *args, **kwargs):
+    #     print("request body :", request.body)  # 실제 서비스라면 logger(장고 기본 지원)을 사용 추천
+    #     print("request POST :", request.POST)
+    #     return super().dispatch(request, *args, **kwargs)
 
 
 # def post_list(request):
